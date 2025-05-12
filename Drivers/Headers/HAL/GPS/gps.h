061671e9df9f7eccff5fe08eef1ac7a2b03786e7
SeifMustafa18 <saifmostafa1854@gmail.com>
2025-05-12 22:46:20 +0300

    Update gps.h

--- Drivers/Headers/HAL/GPS/gps.h
+++ Drivers/Headers/HAL/GPS/gps.h
@@ -7,17 +7,18 @@
 #include <stdio.h>
 #include <math.h>
 
-#define M_PI 3.14159265358979323846f
+#define M_PI 3.14159265359f
 
-#define RADIUS 6371 // Earth radius in kilometers
+#define RADIUS 6371.000000f // Earth's radius in Km
 
 
+//float toRadians(float degree);
 bool validate_GPRMC_string(const char* GPRMC_String);
 bool validate_GPRMC_checksum(const char* GPRMC_String);
-void get_Time(const char* GPRMC_String, char* Time_Buffer);
-void get_Latitude(const char* GPRMC_String, char* Latitude_Buffer);
-void get_Longitude(const char* GPRMC_String, char* Longitude_Buffer);
-void getSpeed(const char* GPRMC_String, char* speed_buffer);
-float Distance(const char* lat1_str, const char* lon1_str, const char* lat2_str, const char* lon2_str);
+void get_Time(char* GPRMC_String, char* Time_Buffer);
+void get_Latitude(char* GPRMC_String, char* Latitude_Buffer);
+void get_Longitude(char* GPRMC_String, char* Longitude_Buffer);
+void getSpeed(char* GPRMC_String, char* speed_buffer);
+//float calc_distance(float lon1, float lat1, float lon2, float lat2);
 
 #endif // GPS_H_INCLUDED
